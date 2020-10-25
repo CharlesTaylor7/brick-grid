c34baf40336991e5d29009341417cfe5ef076ddf
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-10-25 16:38:34 -0400

    Remove extra type variable

--- src/Brick/Grid.hs
+++ src/Brick/Grid.hs
@@ -27,7 +27,7 @@ import Brick.Widgets.Border.Style (BorderStyle)
 import Brick.Grid.TH (suffixLenses)
 
 
-data GridStyle name = GridStyle
+data GridStyle = GridStyle
   { borderStyle :: BorderStyle
   -- ^ Borderstyle to use. Brick.Widgets.Border.Style exports unicode, unicodeRounded, unicodeBolded, & ascii out of the box. Other border styles can be created or modified with the BorderStyle constructor.
   , gridWidth :: Int
@@ -52,7 +52,7 @@ padOrTruncate n t =
       | otherwise -> T.take n t
 
 
-toTileWidget :: GridStyle name -> (Text, AttrName) -> Widget name
+toTileWidget :: GridStyle -> (Text, AttrName) -> Widget name
 toTileWidget gridStyle (text, attrName) =
   let
     cellWidth = gridStyle ^. cellWidthL
@@ -61,13 +61,13 @@ toTileWidget gridStyle (text, attrName) =
       padOrTruncate cellWidth text
 
 
-drawTile :: GridStyle name -> (Int, Int) -> Widget name
+drawTile :: GridStyle -> (Int, Int) -> Widget name
 drawTile = do
   toTile <- view toTileL
   toWidget <- ask toTileWidget
   pure $ toWidget . toTile
 
-drawGrid :: GridStyle name -> Widget name
+drawGrid :: GridStyle -> Widget name
 drawGrid = do
   width  <- view gridWidthL
   height <- view gridHeightL
@@ -83,12 +83,12 @@ drawGrid = do
 
   insertHBorders rows
 
-insertVBorders :: [Widget name] -> GridStyle name -> Widget name
+insertVBorders :: [Widget name] -> GridStyle -> Widget name
 insertVBorders cells = do
   v <- view $ borderStyleL . bsVerticalL . to (txt . T.singleton)
   pure . hBox . (v :) . (<> [v]) . intersperse v $ cells
 
-insertHBorders :: [Widget name] -> GridStyle name -> Widget name
+insertHBorders :: [Widget name] -> GridStyle -> Widget name
 insertHBorders cells = do
   h1 <- hBorder Top
   h2 <- hBorder Bottom
@@ -96,7 +96,7 @@ insertHBorders cells = do
   pure . vBox . (h1 :) . (<> [h2]) . intersperse h3 $ cells
 
 
-hBorder :: VLocation -> GridStyle name -> Widget name
+hBorder :: VLocation -> GridStyle -> Widget name
 hBorder v = do
   cellWidth <- view cellWidthL
   mapWidth <- view gridWidthL
